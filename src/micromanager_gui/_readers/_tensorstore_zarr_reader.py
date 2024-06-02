55d65688a8fa7fa2afa72a4c537660ef1a218cea
fdrgsp <federico.gasparoli@gmail.com>
2024-06-02 14:03:40 -0400

    fix: update tensorstor reader

--- src/micromanager_gui/_readers/_tensorstore_zarr_reader.py
+++ src/micromanager_gui/_readers/_tensorstore_zarr_reader.py
@@ -108,13 +108,15 @@ class TensorstoreZarrReader:
             indexes to a tiff file. If a Mapping of axis and index (e.g.
             {"p": 0, "t": 1}), write the data for the given index to a tiff file.
         """
-        # TODO: add metadata
         if indexers is None:
             if pos := len(self.sequence.stage_positions):
                 with tqdm(total=pos) as pbar:
                     for i in range(pos):
                         data, metadata = self.isel({"p": i}, metadata=True)
                         imwrite(Path(path) / f"p{i}.tif", data, imagej=True)
+                        # save metadata as json
+                        with open(Path(path) / f"p{i}.json", "w") as f:
+                            json.dump(metadata, f)
                         pbar.update(1)
 
         elif isinstance(indexers, list):
@@ -122,17 +124,19 @@ class TensorstoreZarrReader:
                 data, metadata = self.isel(index, metadata=True)
                 name = "_".join(f"{k}{v}" for k, v in index.items())
                 imwrite(Path(path) / f"{name}.tif", data, imagej=True)
+                # save metadata as json
+                with open(Path(path) / f"{name}.json", "w") as f:
+                    json.dump(metadata, f)
 
         else:
             data, metadata = self.isel(indexers, metadata=True)
             imj = len(data.shape) <= 5
-            try:
-                imwrite(path, data, imagej=imj)
-            except IsADirectoryError as e:
-                raise IsADirectoryError(
-                    "The path should be a file path, not a directory! "
-                    "(e.g. 'path/to/file.tif')"
-                ) from e
+            if Path(path).suffix not in {".tif", ".tiff"}:
+                path = Path(path).with_suffix(".tiff")
+            imwrite(path, data, imagej=imj)
+            # save metadata as json
+            with open(Path(path).with_suffix(".json"), "w") as f:
+                json.dump(metadata, f)
 
     # ___________________________Private Methods___________________________
 
