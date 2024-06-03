a138f659a660f2fab0dcecd0d1a42f3061c14360
fdrgsp <federico.gasparoli@gmail.com>
2024-06-03 17:30:33 -0400

    test: wip

--- tests/test_micromanager_gui.py
+++ tests/test_micromanager_gui.py
@@ -1,2 +1,20 @@
-def test_something():
+def test_load_gui():
+    # assert MDA
+    pass
+
+
+def test_snap():
+    pass
+
+
+def test_live():
+    pass
+
+
+def test_mda_viewer():
+    # test that if we save, the datastore of the viewer is the same as the one we saved
+    pass
+
+
+def test_readers():
     pass
