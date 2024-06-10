8af403a747a9ccb84a3bb218ec1c1a3dd35e2a85
fdrgsp <federico.gasparoli@gmail.com>
2024-06-10 16:46:11 -0400

    fix: update stage widget

--- src/micromanager_gui/_widgets/_stage_control.py
+++ src/micromanager_gui/_widgets/_stage_control.py
@@ -1,14 +1,53 @@
 from __future__ import annotations
 
-from typing import cast
+from typing import TYPE_CHECKING
 
 from pymmcore_plus import CMMCorePlus, DeviceType
 from pymmcore_widgets import StageWidget
-from qtpy.QtCore import QMimeData, Qt
-from qtpy.QtGui import QDrag, QDragEnterEvent, QDropEvent, QMouseEvent
-from qtpy.QtWidgets import QGroupBox, QHBoxLayout, QSizePolicy, QWidget
+from qtpy.QtWidgets import (
+    QGridLayout,
+    QGroupBox,
+    QHBoxLayout,
+    QMenu,
+    QSizePolicy,
+    QWidget,
+)
+
+if TYPE_CHECKING:
+    from qtpy.QtGui import QWheelEvent
 
 STAGE_DEVICES = {DeviceType.Stage, DeviceType.XYStage}
+
+
+class Group(QGroupBox):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self._name = name
+
+        self.setLayout(QHBoxLayout())
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setSpacing(0)
+
+        self.setSizePolicy(
+            QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        )
+
+
+class Stage(StageWidget):
+    """Stage control widget with wheel event for z-axis control."""
+
+    def __init__(self, device: str) -> None:
+        super().__init__(device=device)
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        if self._dtype != DeviceType.Stage:
+            return
+        delta = event.angleDelta().y()
+        increment = self._step.value()
+        if delta > 0:
+            self._move_stage(0, increment)
+        elif delta < 0:
+            self._move_stage(0, -increment)
 
 
 class _StagesControlWidget(QWidget):
@@ -19,10 +58,13 @@ class _StagesControlWidget(QWidget):
     ) -> None:
         super().__init__(parent=parent)
 
-        self.setAcceptDrops(True)
-        self.setLayout(QHBoxLayout())
-        self.layout().setContentsMargins(5, 5, 5, 5)
-        self.layout().setSpacing(5)
+        self._stage_wdgs: list[Group] = []
+
+        self._context_menu = QMenu(self)
+
+        self._layout = QGridLayout(self)
+        self._layout.setContentsMargins(5, 5, 5, 5)
+        self._layout.setSpacing(5)
 
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
 
@@ -37,17 +79,17 @@ class _StagesControlWidget(QWidget):
         )
         stage_dev_list = list(self._mmc.getLoadedDevicesOfType(DeviceType.XYStage))
         stage_dev_list.extend(iter(self._mmc.getLoadedDevicesOfType(DeviceType.Stage)))
-        for stage_dev in stage_dev_list:
+        for idx, stage_dev in enumerate(stage_dev_list):
             if self._mmc.getDeviceType(stage_dev) is DeviceType.XYStage:
-                bx = _DragGroupBox("XY Control")
+                bx = Group("XY Control")
             elif self._mmc.getDeviceType(stage_dev) is DeviceType.Stage:
-                bx = _DragGroupBox("Z Control")
+                bx = Group("Z Control")
             else:
                 continue
-            bx.setLayout(QHBoxLayout())
+            self._stage_wdgs.append(bx)
             bx.setSizePolicy(sizepolicy)
-            bx.layout().addWidget(StageWidget(device=stage_dev))
-            self.layout().addWidget(bx)
+            bx.layout().addWidget(Stage(device=stage_dev))
+            self._layout.addWidget(bx, idx // 2, idx % 2)
         self.resize(self.sizeHint())
 
     def _clear(self) -> None:
@@ -56,53 +98,3 @@ class _StagesControlWidget(QWidget):
                 if wdg := item.widget():
                     wdg.setParent(QWidget())
                     wdg.deleteLater()
-
-    def dragEnterEvent(self, event: QDragEnterEvent) -> None:
-        event.accept()
-
-    def dropEvent(self, event: QDropEvent) -> None:
-        pos = event.pos()
-
-        wdgs: list[tuple[int, _DragGroupBox, int, int]] = []
-        zones: list[tuple[int, int]] = []
-        for i in range(self.layout().count()):
-            wdg = cast(_DragGroupBox, self.layout().itemAt(i).widget())
-            wdgs.append((i, wdg, wdg.x(), wdg.x() + wdg.width()))
-            zones.append((wdg.x(), wdg.x() + wdg.width()))
-
-        for idx, w, _, _ in wdgs:
-            if not w.start_pos:
-                continue
-
-            try:
-                curr_idx = next(
-                    (
-                        i
-                        for i, z in enumerate(zones)
-                        if pos.x() >= z[0] and pos.x() <= z[1]
-                    )
-                )
-            except StopIteration:
-                break
-
-            if curr_idx == idx:
-                w.start_pos = 0
-                break
-            cast(QHBoxLayout, self.layout()).insertWidget(curr_idx, w)
-            w.start_pos = 0
-            break
-        event.accept()
-
-
-class _DragGroupBox(QGroupBox):
-    def __init__(self, name: str, start_pos: int = 0) -> None:
-        super().__init__()
-        self._name = name
-        self.start_pos = start_pos
-
-    def mouseMoveEvent(self, event: QMouseEvent) -> None:
-        drag = QDrag(self)
-        mime = QMimeData()
-        drag.setMimeData(mime)
-        self.start_pos = event.pos().x()
-        drag.exec_(Qt.DropAction.MoveAction)
