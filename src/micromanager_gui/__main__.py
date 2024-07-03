ab3d7eb78a54cdf2a440f293cfc8d1f75f90d2be
Talley Lambert <talley.lambert@gmail.com>
2024-07-02 20:32:09 -0400

    lint

--- src/micromanager_gui/__main__.py
+++ src/micromanager_gui/__main__.py
@@ -39,11 +39,12 @@ def main(args: Sequence[str] | None = None) -> None:
 
 def _our_excepthook(
     type: type[BaseException], value: BaseException, tb: TracebackType | None
-):
+) -> None:
     """Excepthook that prints the traceback to the console.
 
     By default, Qt's excepthook raises sys.exit(), which is not what we want.
     """
+    # this could be elaborated to do all kinds of things...
     traceback.print_exception(type, value, tb)
 
 
