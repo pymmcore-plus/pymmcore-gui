e4dbad289ecba8e817f5101c32e19f9eaf68774e
fdrgsp <federico.gasparoli@gmail.com>
2024-10-19 13:59:08 -0400

    test: update

--- tests/test_stage_widget.py
+++ tests/test_stage_widget.py
@@ -12,5 +12,9 @@ if TYPE_CHECKING:
 def test_stage_widget(qtbot: QtBot, global_mmcore: CMMCorePlus, _run_after_each_test):
     s = StagesControlWidget(mmcore=global_mmcore)
     qtbot.addWidget(s)
+    group1 = s._layout.takeAt(0).widget()
+    group2 = s._layout.takeAt(0).widget()
+    assert group1.title() == "XY"
+    assert group2.title() == "Z"
     global_mmcore.loadSystemConfiguration()
     global_mmcore.loadSystemConfiguration()
