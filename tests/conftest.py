cfc0e9cdf130af203cce0f118645b14a5195e838
fdrgsp <federico.gasparoli@gmail.com>
2024-11-05 09:14:50 -0500

    test: fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,15 @@
 from __future__ import annotations
 
+import os
+
+# This is a temporary fix due to a `DeprecationWarning` from the `qtconsole` package:
+# """DeprecationWarning: Jupyter is migrating its paths to use standard platformdirs
+# given by the platformdirs library.  To remove this warning and
+# see the appropriate new directories, set the environment variable
+# `JUPYTER_PLATFORM_DIRS=1` and then run `jupyter --paths`.
+# The use of platformdirs will be the default in `jupyter_core` v6"""
+os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
+
 from pathlib import Path
 from typing import TYPE_CHECKING
 from unittest.mock import patch
