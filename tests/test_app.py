736c51b78b1e38e4bdb43399bbc4953146365ece
Talley Lambert <talley.lambert@gmail.com>
2024-12-23 19:41:19 -0500

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,20 @@
 import sys
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from PyQt6.QtWidgets import QApplication
 
+from pytest import MonkeyPatch
 from pymmcore_gui import _app
 
 
-def test_main_app() -> None:
-    with patch.object(QApplication, "exec") as mock_exec:
-        _app.main()
-        assert mock_exec.called
-        assert isinstance(QApplication.instance(), _app.MMQApplication)
-        assert sys.excepthook == _app.ndv_excepthook
-        for wdg in QApplication.topLevelWidgets():
-            wdg.close()
-            wdg.deleteLater()
+def test_main_app(monkeypatch: MonkeyPatch) -> None:
+    mock_exec = Mock()
+    monkeypatch.setattr(QApplication, "exec", mock_exec)
+    monkeypatch.setattr(sys, 'argv', ['mmgui'])
+    _app.main()
+    mock_exec.assert_called_once()
+    assert isinstance(QApplication.instance(), _app.MMQApplication)
+    assert sys.excepthook == _app.ndv_excepthook
+    for wdg in QApplication.topLevelWidgets():
+        wdg.close()
+        wdg.deleteLater()
