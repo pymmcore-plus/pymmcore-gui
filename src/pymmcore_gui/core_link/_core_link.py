65c253024fbf7adcc8b5d6c431152d85a160e3e5
fdrgsp <federico.gasparoli@gmail.com>
2025-01-20 22:36:28 -0500

    update viewer logic

--- src/pymmcore_gui/core_link/_core_link.py
+++ src/pymmcore_gui/core_link/_core_link.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 from ndv import ArrayViewer
 from pymmcore_plus import CMMCorePlus
-from pymmcore_plus.mda.handlers import TensorStoreHandler
+from pymmcore_plus.mda.handlers import OMETiffWriter, OMEZarrWriter, TensorStoreHandler
 from qtpy.QtCore import QObject
 
 if TYPE_CHECKING:
@@ -24,6 +25,9 @@ class CoreViewersLink(QObject):
         self._mmc = mmcore or CMMCorePlus.instance()
 
         self._current_viewer: ArrayViewer | None = None
+        self._datastore: OMETiffWriter | OMEZarrWriter | TensorStoreHandler | None = (
+            None
+        )
 
         self._mmc.mda.events.sequenceStarted.connect(self._on_sequence_started)
         self._mmc.mda.events.frameReady.connect(self._on_frame_ready)
@@ -34,12 +38,12 @@ class CoreViewersLink(QObject):
     ) -> None:
         """Prepare the acquisition datastore."""
         self._current_viewer = None
+        self._datastore = None
 
-        # pause until the viewer is ready
+        # pause until the datastore is ready
         self._mmc.mda.toggle_pause()
-        print("------------------ PAUSED ------------------")
 
-        # to implement when we will get the datastore form the MDAWidget
+        # TODO: to implement when we will get the datastore form the MDAWidget
         # datastore = self._mda.writer if self._mda is not None else None
         self._datastore = TensorStoreHandler()
         # emit the sequenceStarted signal of the datastore since it is not connected
@@ -50,15 +54,21 @@ class CoreViewersLink(QObject):
 
         # resume the sequence
         self._mmc.mda.toggle_pause()
-        print("------------------ RESUMED ------------------")
 
-    def _on_frame_ready(self, event: MDAEvent) -> None:
+    def _on_frame_ready(self, image: np.ndarray, event: MDAEvent) -> None:
         """Show the MDAViewer when the MDA sequence starts."""
-        print("------------------ FRAME READY ------------------")
-        print(self._current_viewer is None, self._datastore.store is None)
         if self._current_viewer is None and self._datastore.store is not None:
-            self._current_viewer = _ArrayViewer(self._datastore.store)
-            self._current_viewer.show()
+            self._create_ndv_viewer()
+
+        # elif self._current_viewer is not None:
+        #     self._current_viewer.display_model.current_index.update(event.index)
+
+    def _create_ndv_viewer(self) -> None:
+        # TODO: this is triggering the standard TensorstoreWrapper. Remove .store
+        # to use the new TensorstoreWrapper whuch is not working yet
+        self._current_viewer = ArrayViewer(self._datastore.store)
+        # self._current_viewer = ArrayViewer(self._datastore)
+        self._current_viewer.show()
 
     def _on_sequence_finished(self, sequence: useq.MDASequence) -> None:
         """Reset the variables and disconnect the signals."""
@@ -67,21 +77,7 @@ class CoreViewersLink(QObject):
         )
         self._mmc.mda.events.frameReady.disconnect(self._datastore.frameReady)
 
+        # if there is not a viewer, create one (this can happen if the sequence is very
+        # short, like a single channel for example)
         if self._current_viewer is None and self._datastore.store is not None:
-            print("------------------ ADDING AT THE END ------------------")
-            self._current_viewer = _ArrayViewer(self._datastore.store)
-            self._current_viewer.widget().show()
-
-        breakpoint()
-
-        # self._current_viewer = None
-        # self._datastore = None
-
-
-class _ArrayViewer(ArrayViewer):
-    def __init__(self, data, **kwargs):
-        super().__init__(data, **kwargs)
-        print("ArrayViewer created")
-        print("----------------------")
-        print(type(data))
-        print(data)
+            self._create_ndv_viewer()
