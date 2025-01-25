b790a608fdb820608285d807a861b3e02f28f485
Talley Lambert <talley.lambert@gmail.com>
2025-01-25 16:14:13 -0500

    wip

--- src/pymmcore_gui/_sentry.py
+++ src/pymmcore_gui/_sentry.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import functools
 import os
 import platform
@@ -9,12 +11,13 @@ from site import getsitepackages, getusersitepackages
 from subprocess import run
 from typing import TYPE_CHECKING
 
-if not TYPE_CHECKING:
+if TYPE_CHECKING:
+    from sentry_sdk._types import Event, Hint
+else:
     try:
         from rich import print
     except ImportError:  # pragma: no cover
         from pprint import pprint as print
-
 
 SENTRY_DSN = "https://5dd4ce839a4257693e030b136b3ac126@o100671.ingest.us.sentry.io/4507535157952512"
 SHOW_HOSTNAME = os.getenv("MM_TELEMETRY_SHOW_HOSTNAME", "0") in ("1", "True")
@@ -22,7 +25,7 @@ SHOW_LOCALS = os.getenv("MM_TELEMETRY_SHOW_LOCALS", "1") in ("1", "True")
 DEBUG = bool(os.getenv("MM_TELEMETRY_DEBUG"))
 
 
-def strip_sensitive_data(event: dict, hint: dict) -> dict:
+def strip_sensitive_data(event: Event, hint: Hint) -> Event | None:
     """Pre-send hook to strip sensitive data from `event` dict.
 
     https://docs.sentry.io/platforms/python/configuration/filtering/#filtering-error-events
@@ -31,11 +34,12 @@ def strip_sensitive_data(event: dict, hint: dict) -> dict:
 
     # strip `abs_paths` from stack_trace to hide local paths
     with suppress(KeyError, IndexError):
-        for exc in event["exception"]["values"]:
-            for frame in exc["stacktrace"]["frames"]:
-                frame.pop("abs_path", None)
-        # only include the name of the executable in sys.argv (remove pathsƒ)
-        if args := event["extra"]["sys.argv"]:
+        if exception := event.get("exception"):
+            for exc in exception.get("values", []):
+                for frame in exc.get("stacktrace", {}).get("frames", []):
+                    frame.pop("abs_path", None)
+        # only include the name of the executable in sys.argv (remove paths)
+        if (extra := event.get("extra")) and (args := extra.get("sys.argv")):
             args[0] = args[0].split(os.sep)[-1]
     if DEBUG:  # pragma: no cover
         print(event)
@@ -52,7 +56,7 @@ def is_editable_install() -> bool:
     return all(loc not in root for loc in installed_paths)
 
 
-def try_get_git_sha(dist_name: str = "micromanager-gui") -> str:
+def try_get_git_sha(dist_name: str = "pymmcore-gui") -> str:
     """Try to return a git sha, for `dist_name` and detect if dirty.
 
     Return empty string on failure.
@@ -84,44 +88,8 @@ def get_release() -> str:
         if is_editable_install():
             if sha := try_get_git_sha():
                 return sha
-        return metadata.version("micromanager-gui")
+        return metadata.version("pymmcore-gui")
     return "UNDETECTED"
-
-
-# https://docs.sentry.io/platforms/python/configuration/
-SENTRY_SETTINGS = {
-    "dsn": SENTRY_DSN,
-    # When enabled, local variables are sent along with stackframes.
-    # This can have a performance and PII impact.
-    # Enabled by default on platforms where this is available.
-    "include_local_variables": SHOW_LOCALS,
-    # A number between 0 and 1, controlling the percentage chance
-    # a given transaction will be sent to Sentry.
-    # (0 represents 0% while 1 represents 100%.)
-    # Applies equally to all transactions created in the app.
-    # Either this or traces_sampler must be defined to enable tracing.
-    "traces_sample_rate": 1.0,
-    # When provided, the name of the server is sent along and persisted
-    # in the event. For many integrations the server name actually
-    # corresponds to the device hostname, even in situations where the
-    # machine is not actually a server. Most SDKs will attempt to
-    # auto-discover this value. (computer name: potentially PII)
-    "server_name": None if SHOW_HOSTNAME else "",
-    # If this flag is enabled, certain personally identifiable information (PII)
-    # is added by active integrations. By default, no such data is sent.
-    "send_default_pii": False,
-    # This function is called with an SDK-specific event object, and can return a
-    # modified event object or nothing to skip reporting the event.
-    # This can be used, for instance, for manual PII stripping before sending.
-    "before_send": strip_sensitive_data,
-    "debug": DEBUG,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    "profiles_sample_rate": 1.0,
-    # -------------------------
-    "environment": platform.system(),
-}
 
 
 @functools.lru_cache
@@ -156,7 +124,39 @@ def install_error_reporter() -> None:
     if SENTRY_INSTALLED:
         return  # pragma: no cover
 
-    sentry_sdk.init({**SENTRY_SETTINGS, "release": get_release()})
+    sentry_sdk.init(
+        SENTRY_DSN,
+        # When enabled, local variables are sent along with stackframes.
+        # This can have a performance and PII impact.
+        # Enabled by default on platforms where this is available.
+        include_local_variables=SHOW_LOCALS,
+        # A number between 0 and 1, controlling the percentage chance
+        # a given transaction will be sent to Sentry.
+        # (0 represents 0% while 1 represents 100%.)
+        # Applies equally to all transactions created in the app.
+        # Either this or traces_sampler must be defined to enable tracing.
+        traces_sample_rate=1.0,
+        # When provided, the name of the server is sent along and persisted
+        # in the event. For many integrations the server name actually
+        # corresponds to the device hostname, even in situations where the
+        # machine is not actually a server. Most SDKs will attempt to
+        # auto-discover this value. (computer name: potentially PII)
+        server_name=None if SHOW_HOSTNAME else "",
+        # If this flag is enabled, certain personally identifiable information (PII)
+        # is added by active integrations. By default, no such data is sent.
+        send_default_pii=False,
+        # This function is called with an SDK-specific event object, and can return a
+        # modified event object or nothing to skip reporting the event.
+        # This can be used, for instance, for manual PII stripping before sending.
+        before_send=strip_sensitive_data,
+        debug=DEBUG,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # TODO: recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+        release=get_release(),
+        environment=platform.system(),
+    )
     for k, v in get_tags().items():
         sentry_sdk.set_tag(k, v)
     sentry_sdk.set_user({"id": uuid.getnode()})
