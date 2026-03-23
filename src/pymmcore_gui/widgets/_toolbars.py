813434c3fec35e6d72867d6de69c4d5651878f94
fdrgsp <federico.gasparoli@gmail.com>
2026-03-23 11:25:42 -0400

    fix: connect additional events to refresh toolbar on configuration changes

--- src/pymmcore_gui/widgets/_toolbars.py
+++ src/pymmcore_gui/widgets/_toolbars.py
@@ -23,6 +23,8 @@ class OCToolBar(QToolBar):
         mmc.events.channelGroupChanged.connect(self._refresh)
         mmc.events.configSet.connect(self._on_config_set)
         mmc.events.propertyChanged.connect(self._on_property_changed)
+        mmc.events.configDefined.connect(self._refresh)
+        mmc.events.configDeleted.connect(self._refresh)
         self._refresh()
 
     def _on_config_set(self, group: str, config: str) -> None:
